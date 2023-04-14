3711a6fa7f0a60f2cc5e953ea5c02dad6c6b0854
Denis Kolaj <deniskolaj2@gmail.com>
2023-04-15 00:15:20 +0200

    force the VerticalStepper to scroll down every time the state is changed

--- client/src/views/Checkpoint.tsx
+++ client/src/views/Checkpoint.tsx
@@ -2,7 +2,7 @@ import { Grid } from '@mui/material';
 import  SortableListContainer  from '../components/Checkpoint/Challenge/SortableListContainer'
 import { useParams } from 'react-router-dom';
 import VerticalLinearStepper from '../components/Checkpoint/Challenge/Stepper';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { ChallengeInfo } from '../types/checkpoint';
 import ProgressInfo from '../components/Checkpoint/Challenge/ProgressInfo';
 
@@ -10,13 +10,21 @@ import ProgressInfo from '../components/Checkpoint/Challenge/ProgressInfo';
 
 const Checkpoint = () => {
     const params = useParams();
+    const challengeInfoRef = useRef<HTMLDivElement>(null);
     const [challengeInfo, setChallengeInfo] = useState<ChallengeInfo>([]);
     const [activeStep, setActiveStep] = useState<number>(0);
     const [completed, setCompleted] = useState<number>(0);
     const [errorCounter, setErrorCounter] = useState<number>(0);
+
+    useEffect(() => {
+        const container = challengeInfoRef.current;
+        if (container) {
+          container.scrollTop = container.scrollHeight;
+        }
+      }, [challengeInfo]);
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={10}>
+        <Grid container spacing={2} style={{ height: '100%' }}>
+            <Grid item xs={10} >
                 <ProgressInfo completed={completed} errorCounter={errorCounter}/>
             </Grid>
             <Grid item xs={6}>
@@ -28,8 +36,8 @@ const Checkpoint = () => {
                     setErrorCounter={setErrorCounter}  
                 />
             </Grid>
-            <Grid item xs={4}>
-                <VerticalLinearStepper challengeInfo={challengeInfo} activeStep={activeStep}/>
+            <Grid item xs={3} style={{ maxHeight: '400px', overflowY: 'scroll' }} ref={challengeInfoRef} >
+                <VerticalLinearStepper challengeInfo={challengeInfo} activeStep={activeStep} />
             </Grid>
         </Grid>
     );
