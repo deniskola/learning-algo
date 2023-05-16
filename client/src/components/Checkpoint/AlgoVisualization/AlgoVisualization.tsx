5283d977b55b1e74ae940d633312f0cee196fa4c
Denis Kolaj <deniskolaj2@gmail.com>
2023-05-16 23:16:04 +0200

    call the algo visualization function based on current checkpoint

--- client/src/components/Checkpoint/AlgoVisualization/AlgoVisualization.tsx
+++ client/src/components/Checkpoint/AlgoVisualization/AlgoVisualization.tsx
@@ -1,8 +1,83 @@
+import { useSelector } from "react-redux";
+import List from "./List";
+import {useState, useEffect} from "react"
+import { RootState } from "../../../redux/store";
+import { Items, SortingSteps } from "../../../types/checkpoint";
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
 
 
 const AlgoVisualization = () => {
+    const [sortingFunction, setSortingFunction] = useState<Function | null>(null);
+    const currentCheckpoint = useSelector((state: RootState) => state.currentModule.currentCheckpoint);
+    const [showItems, setShowItems] = useState<boolean>(false);
+    const [sortingSteps, setSortingSteps] = useState<SortingSteps>([])
+    const [countTimes, setCountTimes] = useState<number>(0)
+    const [items, setItems] = useState<Items>([
+        {
+            id: 0,
+            value: '1',
+            x: [0, 0],
+            opacity: [0.5]
+        },
+        {
+            id: 1,
+            value: '2',
+            x: [0, 0],
+            opacity: [0.5]
+        },
+        {
+            id: 2,
+            value: '5',
+            x: [0, 0],
+            opacity: [0.5]
+        },
+        {
+            id: 3,
+            value: '2',
+            x: [0, 0],
+            opacity: [0.5]
+        },
+        {
+            id: 4,
+            value: '4',
+            x: [0, 0],
+            opacity: [0.5]
+        },
+        {
+            id: 5,
+            value: '3',
+            x: [0, 0],
+            opacity: [0.5]
+        },
+        {
+            id: 6,
+            value: '1',
+            x: [0, 0],
+            opacity: [0.5]
+        },
+    ])
+
+    useEffect(() => {
+        async function loadSortingFunction() {
+          let algo: any;
+          algo = await import(`../../../helpers/${currentCheckpoint.function}`);
+          setSortingFunction(() => algo.default);
+        }
+        loadSortingFunction();
+    }, [currentCheckpoint.function]);
+    
     return (
-        <>algo-visualization</>
+        <>
+            <Stack direction="row" spacing={2}>
+                <Typography variant="h5" display="block" sx={{ m: 2 }}>
+                    {currentCheckpoint.title}
+                </Typography>
+            </Stack>
+            <List items={items} setCountTimes={setCountTimes} setSortingSteps={setSortingSteps} setShowItems={setShowItems} countTimes={countTimes} showItems={showItems}/>
+            <button onClick={()=> {sortingFunction && sortingFunction(items, setCountTimes,  false, setSortingSteps); setShowItems(true) }}>Sort</button>
+        </>
     );
 }
 
--- client/src/components/Checkpoint/AlgoVisualization/List.tsx
+++ client/src/components/Checkpoint/AlgoVisualization/List.tsx
@@ -1,147 +1,61 @@
-import { useState } from 'react'
 import { ListItem } from './ListItem'
 import { motion } from 'framer-motion'
-import BubbleSort from '../../../helpers/BubbleSort'
-import SelectionSort from '../../../helpers/SelectionSort'
-import { Items, SortingSteps } from '../../../types/checkpoint'
 
-const style = {
-   width: 400,
-   display: 'flex',
-   flexDirection: 'row',
-   alignItems: 'end'
-}
+const List = ({items, countTimes, showItems}:any) => {
+  
+  function keyFrameTimes() {
+    const times: number[] = [];
 
-export interface Item {
-  id: number
-  value: string
-}
+    for (let i = 0; i < countTimes; i++) {
+      const value = (i + 1) / countTimes;
+      times.push(value);
+    }
 
-export interface ContainerState {
-  cards: Item[]
-}
-
-interface Swap {
-    index1: number;
-    index2: number;
-}
-
-const List = () => {
-  const [showItems, setShowItems] = useState<boolean>(false);
-  const [sortingSteps, setSortingSteps] = useState<SortingSteps>([])
-  const [countTimes, setCountTimes] = useState<number>(0)
-  const [items, setItems] = useState<Items>([
-    {
-      id: 0,
-      value: '1',
-      x: [0, 0],
-      opacity: [0.5]
-    },
-    {
-      id: 1,
-      value: '2',
-      x: [0, 0],
-      opacity: [0.5]
-    },
-    {
-      id: 2,
-      value: '5',
-      x: [0, 0],
-      opacity: [0.5]
-    },
-    {
-      id: 3,
-      value: '2',
-      x: [0, 0],
-      opacity: [0.5]
-    },
-    {
-      id: 4,
-      value: '4',
-      x: [0, 0],
-      opacity: [0.5]
-    },
-    {
-      id: 5,
-      value: '3',
-      x: [0, 0],
-      opacity: [0.5]
-    },
-    {
-      id: 6,
-      value: '1',
-      x: [0, 0],
-      opacity: [0.5]
-    },
-  ])
-  const [order, setOrder] = useState<Swap[]>([]);
-//   const [order, setOrder] = useState(cards.map((card) => card.id))
-
-//   useEffect(() => {
-//     setOrder(cards.map((card) => card.id))
-//   }, [cards])
-
-//   useEffect(() => {
-//     console.log(cards.filter((card) => order.includes(card.id)))
-//   }, [order])
-
-function keyFrameTimes() {
-  const times: number[] = [];
-
-  for (let i = 0; i < countTimes; i++) {
-    const value = (i + 1) / countTimes;
-    times.push(value);
+    return times;
   }
 
-  return times;
-}
+  return (
+    <>  
+      {showItems ? (
+      <> 
+        <div 
+          style={{width: 400,
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'end'
+        }}>
+          {items.map((item:any) => (
+            <motion.div
+              animate={{
+                x: item.x,    
+                opacity : item.opacity,
+              }}
+              transition= {{
+                times: keyFrameTimes(),
+                duration: 20,
+              }}
+            >
+              <ListItem key={item.id} value={item.value}/>
+            </motion.div>
+          ))}
+        </div>
+      </>) : (
+      <>
+        <div 
+          style={{width: 400,
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'end'
+        }}>
+          {items.map((item:any) => (
+            <div style={{opacity: 0.5}}>
+              <ListItem key={item.id} value={item.value} />
+            </div>
+          ))}
+        </div>
+      </>)} 
 
-return (
-  <>  
-    <button onClick={()=> (console.log(items))}>items</button>
-    <button onClick={()=> (console.log(countTimes))}>countTimes</button>
-    <button onClick={()=> (console.log(keyFrameTimes()))}>keyFrameTimes()</button>
-    {showItems ? (
-    <> 
-      <div 
-        style={{width: 400,
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'end'
-      }}>
-        {items.map((item) => (
-          <motion.div
-            animate={{
-              x: item.x,    
-              opacity : item.opacity,
-            }}
-            transition= {{
-              times: keyFrameTimes(),
-              duration: 20,
-            }}
-          >
-            <ListItem key={item.id} value={item.value}/>
-          </motion.div>
-         ))}
-      </div>
-    </>) : (
-    <>
-      <div 
-        style={{width: 400,
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'end'
-      }}>
-        {items.map((item) => (
-          <div style={{opacity: 0.5}}>
-            <ListItem key={item.id} value={item.value} />
-          </div>
-        ))}
-      </div>
-    </>)} 
-    <button onClick={()=> {BubbleSort(items, setCountTimes,  false, setSortingSteps); setShowItems(true) }}>Bubble Sort</button>
-    <button onClick={()=> {SelectionSort(items, setCountTimes, false, setSortingSteps); setShowItems(true)}}>Selection Sort</button>
-  </>
+    </>
   )
 }
 
