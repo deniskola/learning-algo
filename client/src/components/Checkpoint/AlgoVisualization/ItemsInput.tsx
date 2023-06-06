9c12318406bb117115440afce1ad2e4152997cc4
Denis Kolaj <deniskolaj2@gmail.com>
2023-06-06 22:52:43 +0200

    transfer handleAddClick function logic to useEffect so it will update the items on input change

--- client/src/components/Checkpoint/AlgoVisualization/ItemsInput.tsx
+++ client/src/components/Checkpoint/AlgoVisualization/ItemsInput.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {Items} from "../../../types/checkpoint";
 
 interface ItemsInputProps {
@@ -9,25 +9,26 @@ const ItemsInput: React.FC<ItemsInputProps> = ({setItems}) => {
   const [inputValue, setInputValue] = useState<string>("");
   const [isValid, setIsValid] = useState<boolean>(true);
 
+  useEffect(() => {
+    if (isValid || inputValue === "") {
+      const inputValues = inputValue.split(",");
+      const newData: Items = inputValues
+        .filter((value) => value.trim() !== "")
+        .map((value, index) => ({
+          id: index,
+          value: value.trim(),
+          x: [0, 0],
+          opacity: [0.5],
+        }));
+      setItems(newData);
+    }
+  }, [inputValue, isValid, setItems]);
+
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const input = event.target.value;
-    //restrict the input to only allow numbers and commas
     const filteredInput = input.replace(/[^0-9,]/g, "");
     setInputValue(filteredInput);
     setIsValid(validateInput(filteredInput));
-  };
-
-  const handleAddClick = () => {
-    const inputValues = inputValue.split(",");
-
-    const newData: Items = inputValues.map((value, index) => ({
-      id: index,
-      value: value.trim(),
-      x: [0, 0],
-      opacity: [0.5],
-    }));
-
-    setItems(newData);
   };
 
   const validateInput = (input: string) => {
@@ -44,7 +45,6 @@ const ItemsInput: React.FC<ItemsInputProps> = ({setItems}) => {
         onChange={handleInputChange}
         pattern="^(?:[1-9]|1[0-9]|20)(?:,(?:[1-9]|1[0-9]|20))*$"
       />
-      <button onClick={handleAddClick}>Add</button>
       {!isValid && <p>Please enter a valid input matching the pattern</p>}
     </div>
   );
